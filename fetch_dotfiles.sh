0324c3ca3c61bccca38aea6891e4ad6598d4c515
paul <paul@pop-os.localdomain>
2023-05-21 00:58:48 -0700

    basic nvim settings

---
+++ fetch_dotfiles.sh
@@ -0,0 +1,6 @@
+
+# >>>> Neovim >>>>
+cp ~/.config/nvim/init.lua ./neovim/.config/nvim/init.lua
+cp -r ~/.config/nvim/lua ./neovim/.config/nvim/lua
+# <<<< Neovim <<<<
+
---
+++ neovim/.config/nvim/lua/settings/base.lua
@@ -0,0 +1,70 @@
+local g = vim.g
+local o = vim.o
+local cmd = vim.cmd
+
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
+
+o.encoding = 'UTF-8'
+
+o.relativenumber = true
+
+-- Allow mouse clicks
+o.mouse = 'a'
+
+-- Show matching parens
+o.showmatch = true
+
+-- Highlight search
+o.hlsearch = true
+
+-- Incremental search
+o.incsearch = true
+
+-- Map <leader> to ','
+g.mapleader = ','
+g.maplocalleader= ','
+
+-- Number of columns occupied by a tab
+o.tabstop = 4
+
+-- Set multiple spaces as tabstops
+o.softtabstop = 4
+
+-- Convert tabs to white spaces
+o.expandtab = 4
+
+-- Width for autoindents
+o.shiftwidth = 4
+
+-- Indent new lines automatically
+o.autoindent = true
+
+-- Bash-like tab completions
+o.wildmode = 'longest,list'
+
+-- 80 column border
+o.cc = 80
+
+-- Set color for border highlight
+cmd('highlight ColorColumn ctermbg=238')
+
+-- Allow auto-indenting depending on file type
+cmd('filetype plugin indent on')
+
+-- Syntax highlighting
+cmd('syntax on')
+
+-- Use system clipboard with +
+cmd('set clipboard+=unnamedplus')
+
+cmd('filetype plugin on')
+
+-- Highlight current cursorline
+o.cursorline = true
+
+-- Speed up scrolling in Vim
+o.ttyfast = true
+
+-- Directory to store backup files
+o.backupdir = '~/.cache/vim'
+
